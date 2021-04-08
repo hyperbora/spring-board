f2e13c8e0d5c6a42e1eb0d65404348b1ca4d89d6
hyperbora <hyperbora@gmail.com>
2021-04-08 23:23:27 +0900

    게시글 조회 테스트 추가

--- src/test/java/io/github/hyperbora/spring/board/BoardRepositoryTest.java
+++ src/test/java/io/github/hyperbora/spring/board/BoardRepositoryTest.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.github.hyperbora.spring.board.domain.Board;
-import io.github.hyperbora.spring.board.domain.Member;
-import io.github.hyperbora.spring.board.domain.Role;
 import io.github.hyperbora.spring.board.persistence.BoardRepository;
 import io.github.hyperbora.spring.board.persistence.MemberRepository;
 
@@ -20,37 +18,49 @@ public class BoardRepositoryTest {
     private BoardRepository boardRepo;
 
     @Test
-    public void testInsert() {
-        Member member1 = new Member();
-        member1.setId("member");
-        member1.setPassword("member123");
-        member1.setName("아무개");
-        member1.setRole(Role.ROLE_MEMBER);
-        member1.setEnabled(true);
-        memberRepo.save(member1);
+    public void testGetBoard() {
+        Board board = boardRepo.findById(1L).get();
 
-        Member member2 = new Member();
-        member2.setId("admin");
-        member2.setPassword("admin123");
-        member2.setName("홍길동");
-        member2.setRole(Role.ROLE_ADMIN);
-        member2.setEnabled(true);
-        memberRepo.save(member2);
-
-        for (int i = 1; i <= 13; i++) {
-            Board board = new Board();
-            board.setMember(member1);
-            board.setTitle(member1.getName() + "가 등록한 게시글 " + i);
-            board.setContent(member1.getName() + "가 등록한 게시글 " + i + "입니다.");
-            boardRepo.save(board);
-        }
-
-        for (int i = 1; i <= 13; i++) {
-            Board board = new Board();
-            board.setMember(member2);
-            board.setTitle(member2.getName() + "가 등록한 게시글 " + i);
-            board.setContent(member2.getName() + "가 등록한 게시글 " + i + "입니다.");
-            boardRepo.save(board);
-        }
+        System.out.println("[ " + board.getSeq() + "번 게시 글 상세 정보 ]");
+        System.out.println("제목\t : " + board.getTitle());
+        System.out.println("작성자\t : " + board.getMember().getName());
+        System.out.println("내용 \t : " + board.getContent());
+        System.out.println("등록일\t : " + board.getCreateDate());
+        System.out.println("조회수 \t : " + board.getCnt());
     }
+
+    // @Test
+    // public void testInsert() {
+    // Member member1 = new Member();
+    // member1.setId("member");
+    // member1.setPassword("member123");
+    // member1.setName("아무개");
+    // member1.setRole(Role.ROLE_MEMBER);
+    // member1.setEnabled(true);
+    // memberRepo.save(member1);
+
+    // Member member2 = new Member();
+    // member2.setId("admin");
+    // member2.setPassword("admin123");
+    // member2.setName("홍길동");
+    // member2.setRole(Role.ROLE_ADMIN);
+    // member2.setEnabled(true);
+    // memberRepo.save(member2);
+
+    // for (int i = 1; i <= 13; i++) {
+    // Board board = new Board();
+    // board.setMember(member1);
+    // board.setTitle(member1.getName() + "가 등록한 게시글 " + i);
+    // board.setContent(member1.getName() + "가 등록한 게시글 " + i + "입니다.");
+    // boardRepo.save(board);
+    // }
+
+    // for (int i = 1; i <= 13; i++) {
+    // Board board = new Board();
+    // board.setMember(member2);
+    // board.setTitle(member2.getName() + "가 등록한 게시글 " + i);
+    // board.setContent(member2.getName() + "가 등록한 게시글 " + i + "입니다.");
+    // boardRepo.save(board);
+    // }
+    // }
 }
