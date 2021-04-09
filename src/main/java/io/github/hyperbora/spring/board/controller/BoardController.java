c826a33820f6a29f49174707c19b32efa8ece181
hyperbora <hyperbora@gmail.com>
2021-04-09 10:16:42 +0900

    게시판 컨트롤러 추가

---
+++ src/main/java/io/github/hyperbora/spring/board/controller/BoardController.java
@@ -0,0 +1,24 @@
+package io.github.hyperbora.spring.board.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.github.hyperbora.spring.board.domain.Board;
+import io.github.hyperbora.spring.board.service.BoardService;
+
+@Controller
+@RequestMapping("/board/")
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @RequestMapping("/getBoardList")
+    public String getBoardList(Model model, Board board) {
+        Page<Board> boardList = boardService.getBoardList(board);
+        model.addAttribute("boardList", boardList);
+        return "board/getBoardList";
+    }
+}
