dfa176107d53ed888ff3491f662400868737268e
hyperbora <hyperbora@gmail.com>
2021-04-09 16:51:42 +0900

    게시글 등록할 때 멤버 정보가 없어서 오류 나오는 부분 수정

--- src/main/java/io/github/hyperbora/spring/board/controller/BoardController.java
+++ src/main/java/io/github/hyperbora/spring/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package io.github.hyperbora.spring.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.github.hyperbora.spring.board.domain.Board;
+import io.github.hyperbora.spring.board.security.SecurityUser;
 import io.github.hyperbora.spring.board.service.BoardService;
 
 @Controller
@@ -36,7 +38,8 @@ public class BoardController {
     }
 
     @PostMapping("/insertBoard")
-    public String insertBoard(Board board) {
+    public String insertBoard(Board board, @AuthenticationPrincipal SecurityUser principal) {
+        board.setMember(principal.getMember());
         boardService.insertBoard(board);
         return "redirect:getBoardList";
     }
