69ff13c97cc8fa838cf50342221a8a5c133a7247
hyperbora <hyperbora@gmail.com>
2021-07-17 14:57:33 +0900

    필요없는 어노테이션 제거

--- src/test/java/io/github/hyperbora/spring/board/BoardRepositoryTest.java
+++ src/test/java/io/github/hyperbora/spring/board/BoardRepositoryTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,7 +20,6 @@ import io.github.hyperbora.spring.board.persistence.BoardRepository;
 import io.github.hyperbora.spring.board.persistence.MemberRepository;
 
 @DataJpaTest
-@AutoConfigureMockMvc
 @TestInstance(Lifecycle.PER_CLASS)
 public class BoardRepositoryTest {
 
